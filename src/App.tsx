a7ffb73a1ded184368a6d0dcc5b34e2fb04b45b5
ahmed <ahmedmijdo2@gmail.com>
2025-04-03 16:21:05 +0200

    add headless ui& modal components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import CardProducts from "./components/CardProducts"
+import Modal from "./components/ui/Modal"
 import { productsList } from "./data"
 
 
@@ -6,6 +7,7 @@ function App() {
   const renderProduct = productsList.map(product => <CardProducts key={product.id} product={product}/>)
   return (
     <main className="container mx-auto">
+      <Modal />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 p-2">
         {renderProduct}
       </div>
